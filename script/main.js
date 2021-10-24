58e5aa53176aabb141a2815ad4948faed0f44a9a
Juarez A. Rodrigues <jdevchan@gmail.com>
2021-10-23 23:04:33 -0300

    finished project

---
+++ script/main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'jdevchan',
+  youtube: 'ChanGeek™',
+  facebook: 'people/Juarez-Rodrigues/100009463330542/',
+  instagram: 'juarezarodrigues',
+  twitter: 'changeekoficial'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
